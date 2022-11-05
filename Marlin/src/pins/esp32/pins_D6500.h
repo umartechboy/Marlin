d6f2349b0cbbe64793d8dd561ffba29a29d4293d
Muhammad Umar Hassan <umar.techboy@gmail.com>
2022-11-05 13:11:08 +0500

    MVP

--- Marlin/src/pins/esp32/pins_D6500.h
+++ Marlin/src/pins/esp32/pins_D6500.h
@@ -38,32 +38,53 @@
 
 #define BOARD_INFO_NAME       "D6500"
 
-#include "pins_ESPA_common.h"
+// Limit Switches
+//
+#define X_STOP_PIN                            34
+#define Y_STOP_PIN                            35
+#define Z_STOP_PIN                            15
 
-// Override pin config for best routing
-// Step pins: [14], [16], [27], [33]
-// Dir, En Pins: [4], [12], [17], [21], [22], [25], [26], [32]
-// End Stops: 15, 34, 35
-// Fan: 13
-// Heater: 2
-// Temp: 36
-// end override pin config for best routing
+//
+// Steppers
+//
 
-#if HAS_TMC_UART
-  //
-  // TMC2209 stepper drivers
-  //
+#define X_STEP_PIN                            32
+#define X_DIR_PIN                             33
+#define X_ENABLE_PIN                          25
 
-  //
-  // Hardware serial 1
-  //
-  #define X_HARDWARE_SERIAL              Serial1
-  #define Y_HARDWARE_SERIAL              Serial1
-  #define Z_HARDWARE_SERIAL              Serial1
-  #define E0_HARDWARE_SERIAL             Serial1
+#define Y_STEP_PIN                            26
+#define Y_DIR_PIN                             27
+#define Y_ENABLE_PIN                          14
 
-  #define TMC_BAUD_RATE 115200
-#endif
+#define Z_STEP_PIN                            16
+#define Z_DIR_PIN                             12
+#define Z_ENABLE_PIN                          4
+
+#define E0_STEP_PIN                           22
+#define E0_DIR_PIN                            17
+#define E0_ENABLE_PIN                         21
+
+//
+// Temperature Sensors
+//
+#define TEMP_0_PIN                            36  // Analog Input
+
+//
+// Heaters / Fans
+//
+#define HEATER_0_PIN                           2
+//#define FAN_PIN                               13 // Can't manually control the fan
+#define HEATER_BED_PIN                         4
+
+//
+// MicroSD card
+//
+#define SD_MOSI_PIN                           23
+#define SD_MISO_PIN                           19
+#define SD_SCK_PIN                            18
+#define SDSS                                   5
+#define USES_SHARED_SPI                           // SPI is shared by SD card with TMC SPI drivers
+
 
 /**
  * Hardware serial pins
