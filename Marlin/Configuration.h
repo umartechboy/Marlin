7d0d67a879d99bd2add2c342b769646fd9e1ed1d
umar.techboy <umar.techboy@gmail.com>
2023-02-03 18:42:56 +0500

    LAatest PID

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -662,9 +662,9 @@
     #define DEFAULT_Ki_LIST {   1.08,   1.08 }
     #define DEFAULT_Kd_LIST { 114.00, 114.00 }
   #else
-  #define DEFAULT_Kp 28.78
-  #define DEFAULT_Ki 2.23
-  #define DEFAULT_Kd 92.65
+  #define DEFAULT_Kp 32.03
+  #define DEFAULT_Ki 3.27
+  #define DEFAULT_Kd 78.44
   #endif
 #endif
 
